ff7abf4294a1f2f5c5d9be5fb5acddc5685e82ad
Dylan <pengdahai216@126.com>
2019-09-29 17:03:45 +0800

    First Test

---
+++ src/entity/TestGit.java
@@ -0,0 +1,41 @@
+package entity;
+
+import java.util.Date;
+
+/**
+ * ClassName TestGit
+ *
+ * @author Dylan
+ * @description 测试
+ * @createDate 2019-09-29 17:01
+ */
+public class TestGit {
+
+    private String id;
+    private String url;
+    private Date date;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
